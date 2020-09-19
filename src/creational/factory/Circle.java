2d9193ba1fcc38fb5482f013b7f61326ab1bc2cd
pawan1773 <joginder.pawan@gmail.com>
2020-09-19 18:01:22 +0530

    Factory pattern

--- src/creational/factory/Circle.java
+++ src/creational/factory/Circle.java
@@ -2,9 +2,8 @@ package creational.factory;
 
 public class Circle implements Shape {
 
-	@Override
-	public void draw() {
-		System.out.println("Inside Circle::draw() method.");
-	}
-
+    @Override
+    public void draw() {
+        System.out.println("Drawing a circle");
+    }
 }
--- src/creational/factory/ShapeFactory.java
+++ src/creational/factory/ShapeFactory.java
@@ -2,15 +2,15 @@ package creational.factory;
 
 public class ShapeFactory {
 
-	public static Shape getShape(final String shapeType) {
-		if ("Rectangle".equalsIgnoreCase(shapeType)) {
-			return new Rectangle();
-		} else if ("Circle".equalsIgnoreCase(shapeType)) {
-			return new Circle();
-		} else if ("Square".equalsIgnoreCase(shapeType)) {
-			return new Square();
-		} else {
-			return null;
-		}
-	}
+    public static Shape getShape(final String shapeType) {
+        if ("Rectangle".equalsIgnoreCase(shapeType)) {
+            return new Rectangle();
+        } else if ("Circle".equalsIgnoreCase(shapeType)) {
+            return new Circle();
+        } else if ("Square".equalsIgnoreCase(shapeType)) {
+            return new Square();
+        } else {
+            return null;
+        }
+    }
 }
